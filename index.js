1380b17b4e4a55dd8c97754a91f777ded45cc027
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2023-10-27 09:34:19 +0700

    d

--- index.js
+++ index.js
@@ -1,58 +1,64 @@
-'use strict';
+"use strict";
 
-const express = require('express')
-var cors = require('cors')
-const uuid = require('uuid')
-const multer = require('multer');
-const { db } = require('./firebase');
-const firebase = require('./firebase');
-const { format } = require('util');
+const express = require("express");
+var cors = require("cors");
+const uuid = require("uuid");
+const multer = require("multer");
+const { db } = require("./firebase");
+const firebase = require("./firebase");
+const { format } = require("util");
 
-const app = express()
-app.use(cors())
-app.use(express.urlencoded({extended: false, limit: '50mb', parameterLimit: 1000000}))
-app.use(express.json({ extended: false, limit: '50mb' }))
+const app = express();
+app.use(cors());
+app.use(
+  express.urlencoded({
+    extended: false,
+    limit: "50mb",
+    parameterLimit: 1000000,
+  })
+);
+app.use(express.json({ extended: false, limit: "50mb" }));
 
 const upload = multer({
-  storage: multer.memoryStorage()
-})
-app.use(upload.single())
-const port = 3000
-
+  storage: multer.memoryStorage(),
+});
+app.use(upload.single());
+const port = 3000;
 
 // [END initialize]
-app.post('/', (req, res) => {
-  return res.send(`It's work`)
-})
+app.post("/", (req, res) => {
+  return res.send(`It's work`);
+});
 
-app.post('/api/upload', (req, res) => {
-  console.log(req)
-  if(!req.file) {
-      return res.status(400).send("Error: No files found")
-  } 
-  const blob = firebase.bucket.file(req.file.originalname)
-  
+app.post("/api/upload", (req, res) => {
+  console.log(req);
+  if (!req.file) {
+    return res.status(400).send("Error: No files found");
+  }
+  const blob = firebase.bucket.file(req.file.originalname);
+
   const blobWriter = blob.createWriteStream({
-      metadata: {
-          contentType: req.file.mimetype
+    metadata: {
+      contentType: req.file.mimetype,
     },
-  })
-  
-  blobWriter.on('error', (err) => {
-      console.log(err)
-  })
-  
-  blobWriter.on('finish', async () => {
+  });
+
+  blobWriter.on("error", (err) => {
+    console.log(err);
+  });
+
+  blobWriter.on("finish", async () => {
     const publicUrl = format(
-      `https://firebasestorage.googleapis.com/v0/b/${firebase.bucket.name}/o/${blob.name.replaceAll(' ', '%20')}?alt=media`
+      `https://firebasestorage.googleapis.com/v0/b/${
+        firebase.bucket.name
+      }/o/${blob.name.replaceAll(" ", "%20")}?alt=media`
     );
     try {
       // Make the file public
       await bucket.file(req.file.originalname).makePublic();
     } catch {
       return res.status(200).send({
-        message:
-          `Uploaded the file successfully: ${req.file.originalname}, but public access is denied!`,
+        message: `Uploaded the file successfully: ${req.file.originalname}, but public access is denied!`,
         url: publicUrl,
       });
     }
@@ -61,28 +67,54 @@ app.post('/api/upload', (req, res) => {
       message: "Uploaded the file successfully: " + req.file.originalname,
       url: publicUrl,
     });
-  })
-  
-  blobWriter.end(req.file.buffer)
-})
+  });
 
-app.post('/api/posts', async (req, res) => {
-  const body = req.body ?? {}
+  blobWriter.end(req.file.buffer);
+});
+
+app.post("/api/posts", async (req, res) => {
+  const body = req.body ?? {};
   try {
-    const id = uuid.v4()
-    const result = await db.collection('posts').doc(id).set({
-      _id: id,
-      ...body
-    })
+    const id = uuid.v4();
+    const result = await db
+      .collection("posts")
+      .doc(id)
+      .set({
+        _id: id,
+        createdAt: new Date().valueOf(),
+        ...body,
+      });
 
-    res.json(result.writeTime)
+    res.json(result.writeTime);
   } catch (e) {
-    res.status(500).json(e)
+    res.status(500).json(String(e));
   }
-})
+});
+
+app.get("/api/posts", async (req, res) => {
+  try {
+    const page = Number(req.query?.page ?? 0);
+    const limit = Number(req.query?.limit ?? 10);
+
+    const posts = db.collection("posts");
+
+    const result = await posts.orderBy("createdAt").orderBy('desc').startAt(page).limit(limit).get();
+
+    const total = (await posts.get()).size;
+
+    const data = [];
+    result.forEach((doc) => {
+      data.push(doc.data());
+    });
+
+    res.json({ data, total, page, limit });
+  } catch (e) {
+    res.status(500).json(String(e));
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
 module.exports = app;
